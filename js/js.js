64b51f2593066ce51196950439332e133a4e0a33
helen2325 <33185662+helen2325@users.noreply.github.com>
2017-12-09 17:05:19 +0300

    На допуск

--- js/js.js
+++ js/js.js
@@ -1,5 +1,4 @@
 var link = document.querySelector(".for-form");
 var popup = document.querySelector(".modal-form");
 
-
         link.addEventListener("click", function (evt) { evt.preventDefault(); popup.classList.toggle("modal-show"); });
---
+++ js/min-js.js
@@ -0,0 +1,1 @@
+var link=document.querySelector(".for-form"),popup=document.querySelector(".modal-form");link.addEventListener("click",function(a){a.preventDefault(),popup.classList.toggle("modal-show")});
